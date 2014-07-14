c4e4b6a4d15228a91cb39687de685224e9968917
D3ZOMBKEELA <T10Cunningham@gmail.com>
2014-07-14 15:12:12 -0400

    Initial mod commit. Added empty mod class and mcmod.info file.

---
+++ Elementalist/source/com/rocketoranges/elementalist/Elementalist.java
@@ -0,0 +1,33 @@
+package com.rocketoranges.elementalist;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "Elementalist", name = "Elementalist", version = "1.0a")
+public class Elementalist
+{
+	
+	@Mod.Instance("Elementalist")
+	public static Elementalist instance;
+	
+	@Mod.EventHandler
+	public void PreInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void Init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler
+	public void PostInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+	
+}
